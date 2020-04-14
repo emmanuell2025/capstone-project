50beae197a5c0e9fd2dabe23ff28e8febb8c4b8a
Emmanuel Lopez <emmanuell2025@gmail.com>
2020-04-14 14:09:30 -0400

    Tried using curves to make sound effects

--- Capston_Project/Assets/Scripts/PlayerSounds.cs
+++ Capston_Project/Assets/Scripts/PlayerSounds.cs
@@ -7,15 +7,33 @@ public class PlayerSounds : MonoBehaviour
 
     public AudioClip footstep;
     public AudioSource audioS;
+    private Animator anim;
+    public float currentLeftFootstep;
+    public float currentRightFootstep;
+    public float lastLeftFootstep;
+    public float lastRightFootstep;
     // Start is called before the first frame update
     void Start()
     {
-
+    anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+      currentLeftFootstep = anim.GetFloat("leftFootstep");
+      Debug.Log(currentLeftFootstep);
+      if (currentLeftFootstep > 0 && lastLeftFootstep < 0){
+        Footsteps();
+      }
+      lastLeftFootstep = anim.GetFloat("leftFootstep");
+
+      currentRightFootstep = anim.GetFloat("rightFootstep");
+      Debug.Log(currentRightFootstep);
+      if (currentRightFootstep < 0 && lastRightFootstep > 0){
+        Footsteps();
+      }
+      lastRightFootstep = anim.GetFloat("rightFootstep");
 
     }
 
