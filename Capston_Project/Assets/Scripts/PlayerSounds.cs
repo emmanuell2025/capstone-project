67bb3a44367383337a43ecc66467676d259206ca
Emmanuel Lopez <emmanuell2025@gmail.com>
2020-04-15 18:41:59 -0400

    Fixed walking sound and added punching sound

--- Capston_Project/Assets/Scripts/PlayerSounds.cs
+++ Capston_Project/Assets/Scripts/PlayerSounds.cs
@@ -6,12 +6,13 @@ public class PlayerSounds : MonoBehaviour
 {
 
     public AudioClip footstep;
-    public AudioSource audioS;
+    public AudioClip punch;
+    public AudioClip sword;
+    public AudioSource feetAudioSource;
+    public AudioSource weaponAudioSource;
     private Animator anim;
-    public float currentLeftFootstep;
-    public float currentRightFootstep;
-    public float lastLeftFootstep;
-    public float lastRightFootstep;
+    private bool combat;
+    public float movementSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,23 +22,40 @@ public class PlayerSounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      currentLeftFootstep = anim.GetFloat("leftFootstep");
-      Debug.Log(currentLeftFootstep);
-      if (currentLeftFootstep > 0 && lastLeftFootstep < 0){
-        Footsteps();
-      }
-      lastLeftFootstep = anim.GetFloat("leftFootstep");
+      // currentLeftFootstep = anim.GetFloat("leftFootstep");
+      // Debug.Log(currentLeftFootstep);
+      // if (currentLeftFootstep > 0 && lastLeftFootstep < 0){
+      //   Footsteps();
+      // }
+      // lastLeftFootstep = anim.GetFloat("leftFootstep");
+      //
+      // currentRightFootstep = anim.GetFloat("rightFootstep");
+      // Debug.Log(currentRightFootstep);
+      // if (currentRightFootstep < 0 && lastRightFootstep > 0){
+      //   Footsteps();
+      // }
+      // lastRightFootstep = anim.GetFloat("rightFootstep");
+      movementSpeed = anim.GetFloat("speedPercent");
+      combat = anim.GetBool("inCombat");
 
-      currentRightFootstep = anim.GetFloat("rightFootstep");
-      Debug.Log(currentRightFootstep);
-      if (currentRightFootstep < 0 && lastRightFootstep > 0){
-        Footsteps();
-      }
-      lastRightFootstep = anim.GetFloat("rightFootstep");
 
     }
 
     void Footsteps(){
-      audioS.PlayOneShot(footstep);
+      if (movementSpeed > 0.1){
+        feetAudioSource.PlayOneShot(footstep);
+      }
+    }
+
+    void PunchSound(){
+      if (combat == true){
+        weaponAudioSource.PlayOneShot(punch);
+      }
+    }
+
+    void SwordSound(){
+      if (combat == true){
+        weaponAudioSource.PlayOneShot(sword);
+      }
     }
 }
